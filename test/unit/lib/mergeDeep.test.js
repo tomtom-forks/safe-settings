2c4008904dd6f2fd4e55aa2a6b2c299d62991fda
Martin Migasiewicz <71527391+martinmigasiewicz-tomtom@users.noreply.github.com>
2024-09-12 14:15:30 +0200

    fix(autolinks): mark autolink as changed when url template changes

--- test/unit/lib/mergeDeep.test.js
+++ test/unit/lib/mergeDeep.test.js
@@ -837,14 +837,14 @@ entries:
     const source = YAML.load(`
 entries:
 - key_prefix: ASDF-
-  url_template: https://jira.company.com/browse/ASDF-<num>
+  url_template: https://jiranew.company.com/browse/ASDF-<num>
 - key_prefix: BOLIGRAFO-
   url_template: https://jira.company.com/browse/BOLIGRAFO-<num>
   `)
     const target = YAML.load(`
     entries:
     - key_prefix: ASDF-
-      url_template: https://jiranew.company.com/browse/ASDF-<num>
+      url_template: https://jira.company.com/browse/ASDF-<num>
     - key_prefix: BOLSIGRAFO-
       url_template: https://jira.company.com/browse/BOLIGRAFO-<num>
       `)
@@ -858,6 +858,12 @@ entries:
         ]
       },
       modifications: {
+        entries: [
+          {
+            key_prefix: "ASDF-",
+            url_template: "https://jiranew.company.com/browse/ASDF-<num>"
+          }
+        ]
       },
       deletions: {
         entries: [ {
@@ -865,6 +871,44 @@ entries:
           url_template: "https://jira.company.com/browse/BOLIGRAFO-<num>"
         },]
       },
+      hasChanges: true
+    }
+    const ignorableFields = []
+    const mockReturnGitHubContext = jest.fn().mockReturnValue({
+      request: () => {},
+    });
+    const mergeDeep = new MergeDeep(
+      log,
+      mockReturnGitHubContext,
+      ignorableFields
+    );
+    const merged = mergeDeep.compareDeep(target, source)
+    console.log(`diffs ${JSON.stringify(merged, null, 2)}`)
+    expect(merged).toEqual(expected)
+  })
+
+  it('Autolinks different url template', () => {
+    const source = YAML.load(`
+entries:
+- key_prefix: ASDF-
+  url_template: https://jiranew.company.com/browse/ASDF-<num>
+  `)
+    const target = YAML.load(`
+    entries:
+    - key_prefix: ASDF-
+      url_template: https://jira.company.com/browse/ASDF-<num>
+      `)
+    const expected = {
+      additions: {},
+      modifications: {
+        entries: [
+          {
+            key_prefix: "ASDF-",
+            url_template: "https://jiranew.company.com/browse/ASDF-<num>"
+          }
+        ]
+      },
+      deletions: {},
       hasChanges: true
     }
     const ignorableFields = []
